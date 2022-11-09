d6ce8aa56974d594b289affa0210cfb17f13b42f
Geeknerd1337 <geeknerd1337@gmail.com>
2022-11-08 20:39:56 -0600

    Add Lerp Helper Function

--- My Game/Helpers.h
+++ My Game/Helpers.h
@@ -10,6 +10,7 @@ const Vector2 AngleToVector(const float theta); ///< Convert angle to vector.
 const Vector2 VectorNormalCC(const Vector2& v); ///< Counterclockwise normal.
 const Vector2 RotateVector(const Vector2&, float); ///< Rotate a vector.
 void NormalizeAngle(float& theta); ///< Normalize angle to \f$\pm\pi\f$.
+float Lerp(float, float, float);
 
 /// <summary>
 /// Allows us to see if one type is an instance of another.
@@ -18,5 +19,7 @@ template<typename Base, typename T>
 inline bool instanceof(const T* ptr) {
 	return dynamic_cast<const Base*>(ptr) != nullptr;
 }
+
+
 
 #endif //__L4RC_GAME_HELPERS_H__

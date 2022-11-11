0c7c18c758e2292134a482438d066ab420e20670
Geeknerd1337 <geeknerd1337@gmail.com>
2022-11-11 12:36:08 -0600

    Decrease World Size

--- My Game/Game.cpp
+++ My Game/Game.cpp
@@ -139,8 +139,8 @@ void CGame::Release() {
 
 void CGame::CreateObjects() {
 	//m_pRenderer->GetSize(eSprite::Background, m_vWorldSize.x, m_vWorldSize.y); //init m_vWorldSize
-	m_vWorldSize.x = 4096;
-	m_vWorldSize.y = 4096;
+	m_vWorldSize.x = 1024;
+	m_vWorldSize.y = 1024;
 
 	m_pPlayer = (CPlayer*)m_pObjectManager->create(eSprite::Player_Idle, Vector2(64.0f, 64.0f));
 	//reassigning player like this breaks the camera following

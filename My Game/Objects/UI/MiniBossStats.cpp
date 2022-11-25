d59cae2826267dd1360321bc2e215c62ef42dd3a
FireAndIrons <67036557+FireAndIrons@users.noreply.github.com>
2022-11-24 20:31:29 -0600

    tuning mini boss health bar

--- My Game/Objects/UI/MiniBossStats.cpp
+++ My Game/Objects/UI/MiniBossStats.cpp
@@ -16,7 +16,7 @@ void MiniBossStats::DrawHealthBar() {
 	CMiniBoss* m_pMiniBoss = (CMiniBoss*)d;
 
 	if (m_pMiniBoss != nullptr) {
-		float x = (m_pCanvas->m_nWidth / 5.0f) - 30.0f;
+		float x = (1280.0f / 5.0f) - 30.0f;
 		float y = 600.0f;
 		float w = 832.0f;
 		float h = 25.0f;
@@ -25,11 +25,11 @@ void MiniBossStats::DrawHealthBar() {
 		std::string hp = "HP";
 		std::string name = "Baron Zintius";
 		UIHelpers::DrawAlignedText(hp.c_str(), Vector2(x, y + h / 2.0f + 3.0f), HorizontalAlignment::Left, VerticalAlignment::Center, Colors::White);
-		UIHelpers::DrawAlignedText(name.c_str(), Vector2(x, y + h / 2.0f + 3.0f), HorizontalAlignment::Left, VerticalAlignment::Top, Colors::DarkMagenta);
+		UIHelpers::DrawAlignedText(name.c_str(), Vector2(x, y + h / 2.0f + 3.0f), HorizontalAlignment::Left, VerticalAlignment::Top, Colors::BlueViolet);
 		float vertStatPadding = 10.0f;
 		Vector2 pos = Vector2(x + 35.0f, y + vertStatPadding / 2.0f);
 		Vector2 size = Vector2(315.0f, h - vertStatPadding);
-		UIHelpers::DrawRectangle(pos, size, Vector4(111.0 / 255.0f, 27.0 / 255.0f, 43.0 / 255.0f, 1.0f));
+		UIHelpers::DrawRectangle(pos, size, Vector4(0, 1, 0, 1));
 		UIHelpers::DrawStatBar(pos, size, m_pMiniBoss->GetDisplayLastHealth(), 100.0f, Vector4(1,0,0,1));
 		UIHelpers::DrawStatBar(pos, size, m_pMiniBoss->GetDisplayHealth(), 100.0f, Vector4(190.0/255.0, 39.0/255.0, 67.0/255.0, 1.0));
 	}
